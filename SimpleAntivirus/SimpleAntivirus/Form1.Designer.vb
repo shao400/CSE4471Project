aa02a3ae9feaee53853c6784f410dbb268c31802
Zilin Shao <shao.400@osu.edu>
2020-10-20 01:52:27 +0800

    First Update

---
+++ SimpleAntivirus/SimpleAntivirus/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAuthor = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtHash = New System.Windows.Forms.TextBox()
+        Me.lblMD5 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'lblAuthor
+        '
+        Me.lblAuthor.AutoSize = True
+        Me.lblAuthor.Font = New System.Drawing.Font("Mistral", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAuthor.Location = New System.Drawing.Point(501, 94)
+        Me.lblAuthor.Name = "lblAuthor"
+        Me.lblAuthor.Size = New System.Drawing.Size(62, 26)
+        Me.lblAuthor.TabIndex = 0
+        Me.lblAuthor.Text = "By Z.S."
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(192, 43)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(371, 51)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Simple Anti-Virus"
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(22, 148)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(639, 31)
+        Me.txtFilePath.TabIndex = 2
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(682, 143)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(97, 40)
+        Me.btnBrowse.TabIndex = 3
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtHash
+        '
+        Me.txtHash.Location = New System.Drawing.Point(132, 210)
+        Me.txtHash.Name = "txtHash"
+        Me.txtHash.Size = New System.Drawing.Size(256, 31)
+        Me.txtHash.TabIndex = 4
+        '
+        'lblMD5
+        '
+        Me.lblMD5.AutoSize = True
+        Me.lblMD5.Location = New System.Drawing.Point(35, 216)
+        Me.lblMD5.Name = "lblMD5"
+        Me.lblMD5.Size = New System.Drawing.Size(63, 25)
+        Me.lblMD5.TabIndex = 5
+        Me.lblMD5.Text = "MD5:"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(16, 270)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(98, 31)
+        Me.lblStatus.TabIndex = 6
+        Me.lblStatus.Text = "Status"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(140, 276)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 25)
+        Me.lblResult.TabIndex = 7
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 324)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.lblMD5)
+        Me.Controls.Add(Me.txtHash)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.lblAuthor)
+        Me.Name = "Form1"
+        Me.Text = "Simple Anti-Virus"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtHash As TextBox
+    Friend WithEvents lblMD5 As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
