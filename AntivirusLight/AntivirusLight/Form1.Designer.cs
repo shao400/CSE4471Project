2219db47f26f728fa1db6cfc6dcc5505e6268b33
Zilin Shao <66044248+shao400@users.noreply.github.com>
2020-11-24 01:36:55 +0800

    AntivirusLight

---
+++ AntivirusLight/AntivirusLight/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace AntivirusLight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.bttnScan = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.lblMD5 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bttnBrowse = new System.Windows.Forms.Button();
+            this.bttnCopy = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(106, 73);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(355, 31);
+            this.txtFile.TabIndex = 0;
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(106, 140);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.Size = new System.Drawing.Size(355, 31);
+            this.txtMD5.TabIndex = 1;
+            // 
+            // bttnScan
+            // 
+            this.bttnScan.Location = new System.Drawing.Point(487, 134);
+            this.bttnScan.Name = "bttnScan";
+            this.bttnScan.Size = new System.Drawing.Size(103, 37);
+            this.bttnScan.TabIndex = 2;
+            this.bttnScan.Text = "Scan";
+            this.bttnScan.UseVisualStyleBackColor = true;
+            this.bttnScan.Click += new System.EventHandler(this.bttnScan_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(34, 73);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(47, 25);
+            this.lblFile.TabIndex = 3;
+            this.lblFile.Text = "File";
+            // 
+            // lblMD5
+            // 
+            this.lblMD5.AutoSize = true;
+            this.lblMD5.Location = new System.Drawing.Point(34, 140);
+            this.lblMD5.Name = "lblMD5";
+            this.lblMD5.Size = new System.Drawing.Size(57, 25);
+            this.lblMD5.TabIndex = 4;
+            this.lblMD5.Text = "MD5";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(101, 215);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(120, 25);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status: N/A";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(623, 40);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(72, 36);
+            this.Menu.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 44);
+            this.exitToolStripMenuItem.Text = "History";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(224, 44);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mistral", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(539, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "By Z.S.";
+            // 
+            // bttnBrowse
+            // 
+            this.bttnBrowse.Location = new System.Drawing.Point(487, 70);
+            this.bttnBrowse.Name = "bttnBrowse";
+            this.bttnBrowse.Size = new System.Drawing.Size(103, 37);
+            this.bttnBrowse.TabIndex = 8;
+            this.bttnBrowse.Text = "Browse";
+            this.bttnBrowse.UseVisualStyleBackColor = true;
+            this.bttnBrowse.Click += new System.EventHandler(this.bttnBrowse_Click);
+            // 
+            // bttnCopy
+            // 
+            this.bttnCopy.Location = new System.Drawing.Point(487, 201);
+            this.bttnCopy.Name = "bttnCopy";
+            this.bttnCopy.Size = new System.Drawing.Size(103, 39);
+            this.bttnCopy.TabIndex = 9;
+            this.bttnCopy.Text = "Copy";
+            this.bttnCopy.UseVisualStyleBackColor = true;
+            this.bttnCopy.Click += new System.EventHandler(this.bttnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 265);
+            this.Controls.Add(this.bttnCopy);
+            this.Controls.Add(this.bttnBrowse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblMD5);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.bttnScan);
+            this.Controls.Add(this.txtMD5);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Basic AntiVirus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.Button bttnScan;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label lblMD5;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bttnBrowse;
+        private System.Windows.Forms.Button bttnCopy;
+    }
+}
+
---
+++ AntivirusLight/AntivirusLight/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntivirusLight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        /* Function used to get MD5 from a file */
+        private string GetMD5FromFile(String filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    /* Return MD5 and present it with every "-" removed and to lower case */
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-",string.Empty).ToLower();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnScan_Click(object sender, EventArgs e)
+        {
+            var md5Signatures = File.ReadAllLines("MD5Base.txt");
+            if (md5Signatures.Contains(txtMD5.Text))
+            {
+                lblStatus.Text = "File Infected!";
+                lblStatus.ForeColor = Color.Red;
+            }
+            else 
+            {
+                lblStatus.Text = "File Clean!";
+                lblStatus.ForeColor = Color.Green;
+            }
+        }
+   
+
+        /* Browse button */
+        private void bttnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "Textfiles | *.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo f = new FileInfo(ofd.FileName);
+                txtFile.Text = f.FullName;
+                txtMD5.Text = GetMD5FromFile(ofd.FileName); /* File text box*/
+            }
+            lblStatus.Text = "N/A";
+            lblStatus.ForeColor = Color.Black;
+        }
+
+        /* Copy MD5 to Clipboard */
+        private void bttnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtMD5.Text);
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
